aac9c9e2f32530478fb8ba59132eff12af9e51e0
Richard Flick <richard.flick@nhs.net>
2019-01-15 09:55:09 +0000

    :tada: passing string prob

--- app/routes.js
+++ app/routes.js
@@ -14,6 +14,8 @@ router.get('/', function(req , res){
 
 
 // Branching
+
+// To Level 2
 router.post('/reasonforcontact-answer', function (req, res) {
   let reason = req.body.reasonforcontact;
 
@@ -23,15 +25,42 @@ router.post('/reasonforcontact-answer', function (req, res) {
   else if (reason == "feedback"){
     res.redirect('/contact-us/level-2-feedback')
   }
-  else if (reason == "feedback"){
+  else if (reason == "complaint"){
     res.redirect('/contact-us/level-2-complaint')
   }
-  else if (reason == "feedback"){
+  else if (reason == "pims"){
     res.redirect('/contact-us/level-2-pims')
   }
+
+// To Level 3 - Healthcare Data Query
+  else if (reason == "medical-records"){
+    res.redirect('/contact-us/level-3-healthcare-medicalrecords')
+  }
+  else if (reason == "request-nhs-number"){
+    res.redirect('/contact-us/level-3-healthcare-nhsnumber')
+  }
+  else if (reason == "personal-medical-enquiry"){
+    res.redirect('/contact-us/level-3-healthcare-medicalenquiry')
+  }
+
+ // To Level 3 - Feedback
+  else if (reason == "nhsuk-website"){
+    res.redirect('/contact-us/level-3-feedback-nhsukwebsite')
+  }
+  else if (reason == "nhs-app"){
+    res.redirect('/contact-us/level-3-feedback-nhsukwebsite2?reason=nhs-app')
+  }
+  else if (reason == "nhs-service"){
+    res.redirect('/contact-us/level-3-feedback-nhsukwebsite2')
+  }
+
   else {
     res.redirect('/')
   }
+});
+
+router.get('/contact-us/level-3-feedback-nhsukwebsite2', function (req, res) {
+    var reason = req.reason;
 });
 
 module.exports = router;
